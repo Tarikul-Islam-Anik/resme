d2c3c1caed39b5eca96136d70b994175a2558179
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-27 21:19:43 +0600

    Add profile page component

---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,34 @@
+import { getServerSession } from 'next-auth';
+
+import { Box } from '@/components/layout/box';
+import Loader from '@/components/shared/loader';
+import prisma from '@/prisma';
+
+import ProfileForm from './form';
+
+import SectionHeading from '../components/section-heading';
+
+async function getUserData() {
+  const session = await getServerSession();
+  return prisma.user.findUnique({
+    where: {
+      email: session?.user?.email!,
+    },
+  });
+}
+
+export default async function ProfilePage() {
+  const user = await getUserData();
+
+  return (
+    <Box className='space-y-6'>
+      <SectionHeading
+        title='Profile'
+        description='This information will be displayed publicly so be careful what you share.'
+      />
+      {user ? <ProfileForm user={user} /> : <Loader />}
+    </Box>
+  );
+}
+
+ProfilePage.displayName = 'ProfilePage';
