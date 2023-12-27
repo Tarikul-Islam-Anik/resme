ebe0264c67b3eabcc03c5618d6634b1b46118a8a
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-27 21:19:57 +0600

    Add dashboard page component

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Download, MousePointerClick } from 'lucide-react';
+import { getServerSession } from 'next-auth';
+
+import { Box } from '@/components/layout/box';
+import { Flex } from '@/components/layout/flex';
+import prisma from '@/prisma';
+
+import DeleteAccount from './components/delete-account';
+import SectionHeading from './components/section-heading';
+import StatusCard from './components/status-card';
+
+async function getAnalytics() {
+  const session = await getServerSession();
+  return prisma.analytics.findMany({
+    where: {
+      userEmail: session?.user?.email!,
+    },
+    select: {
+      clicks: true,
+      downloads: true,
+    },
+  });
+}
+const DashBoardPage = async () => {
+  const analytics = await getAnalytics();
+
+  const formatNumber = (num: number) => new Intl.NumberFormat().format(num);
+
+  return (
+    <Box className='space-y-6'>
+      <SectionHeading
+        title='Dashboard'
+        description='Check your resume performance and manage your account.'
+      />
+      <Flex gap={4} direction='column' className='sm:flex-row'>
+        <StatusCard
+          title='Total clicks'
+          value={analytics.length ? formatNumber(analytics[0].clicks) : 0}
+          icon={
+            <MousePointerClick className='roa h-5 w-5 text-muted-foreground' />
+          }
+          description='Lifetime views of your resume.'
+        />
+        <StatusCard
+          title='Total downloads'
+          value={analytics.length ? formatNumber(analytics[0].downloads) : 0}
+          icon={<Download className='h-5 w-5 text-muted-foreground' />}
+          description='People downloaded your resume.'
+        />
+      </Flex>
+      <DeleteAccount />
+    </Box>
+  );
+};
+
+DashBoardPage.displayName = 'DashBoardPage';
+export default DashBoardPage;
