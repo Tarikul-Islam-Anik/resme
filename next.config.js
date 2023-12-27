6aecb3f34114c5bef52a7466bf59703088702015
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-27 21:15:21 +0600

    Add remotePatterns for Cloudinary images in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
 
 module.exports = withBundleAnalyzer(nextConfig);
