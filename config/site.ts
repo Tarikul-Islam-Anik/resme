7672cfadd2ed2efd9bc920b980c25648f4df679b
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-28 12:11:56 +0600

    Add site configuration file

---
+++ config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: 'Resme',
+  url: new URL('https://resme.vercel.app'),
+  description:
+    'Resme is a free resume builder that helps you create a beautiful looking resume online.',
+};
+
+export type SiteConfig = typeof siteConfig;
