e5cca702fe089990a76e8d8ab325b1aa3e693d44
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 00:06:58 +0600

    Add GitHub and owner links to site config

--- config/site.ts
+++ config/site.ts
@@ -3,6 +3,8 @@ export const siteConfig = {
   url: new URL('https://resme.vercel.app'),
   description:
     'Resme is a free resume builder that helps you create a beautiful looking resume online.',
+  github: 'https://github.com/Tarikul-Islam-Anik/resme',
+  owner: 'https://oxyzen.vercel.app',
 };
 
 export type SiteConfig = typeof siteConfig;
