8ff59d7065254b046c7df093d003abb9467dcbeb
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 00:36:07 +0600

    Refactor header component to conditionally render navigation items

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -22,42 +22,45 @@ import { dashboardNavItems } from '../dashboard/components/dashboard-navitems';
 
 const Header = () => {
   const pathname = usePathname();
+  const show = pathname === '/' || pathname.includes('dashboard');
   return (
-    <header className='absolute left-0 top-0 z-50 w-full'>
-      <nav aria-label='Global'>
-        <Flex align='center' justify='between' className='p-6 lg:px-8'>
-          <Link href='/' className='-m-1.5 p-1.5'>
-            <Flex align='center' gap={2}>
-              <Logo className='h-8 w-8 rounded-md' />
-              <Text size='xl' className='hidden lg:block'>
-                <Strong>Resme</Strong>
-              </Text>
-            </Flex>
-          </Link>
-          <Sheet>
-            {pathname !== '/' ? (
-              <SheetTrigger asChild>
-                <Button size='icon' variant='ghost'>
-                  <Bars2Icon className='h-7 w-7' />
-                </Button>
-              </SheetTrigger>
-            ) : null}
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Navigations</SheetTitle>
-              </SheetHeader>
-              {dashboardNavItems.map((item) => (
-                <Link href={item.href} key={item.href}>
-                  <Text size='sm' className='block p-4'>
-                    {item.title}
-                  </Text>
-                </Link>
-              ))}
-            </SheetContent>
-          </Sheet>
-        </Flex>
-      </nav>
-    </header>
+    show && (
+      <header className='absolute left-0 top-0 z-50 w-full'>
+        <nav aria-label='Global'>
+          <Flex align='center' justify='between' className='p-6 lg:px-8'>
+            <Link href='/' className='-m-1.5 p-1.5'>
+              <Flex align='center' gap={2}>
+                <Logo className='h-8 w-8 rounded-md' />
+                <Text size='xl' className='hidden lg:block'>
+                  <Strong>Resme</Strong>
+                </Text>
+              </Flex>
+            </Link>
+            <Sheet>
+              {pathname.includes('dashboard') ? (
+                <SheetTrigger asChild>
+                  <Button size='icon' variant='ghost'>
+                    <Bars2Icon className='h-7 w-7' />
+                  </Button>
+                </SheetTrigger>
+              ) : null}
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Navigations</SheetTitle>
+                </SheetHeader>
+                {dashboardNavItems.map((item) => (
+                  <Link href={item.href} key={item.href}>
+                    <Text size='sm' className='block p-4'>
+                      {item.title}
+                    </Text>
+                  </Link>
+                ))}
+              </SheetContent>
+            </Sheet>
+          </Flex>
+        </nav>
+      </header>
+    )
   );
 };
 
