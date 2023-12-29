046dbf4687d19fe333ff4ac9056a081a0ec626b6
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 00:07:01 +0600

    Add icon component

---
+++ app/icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/og';
+
+import Logo from '@/components/shared/logo';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Logo />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
