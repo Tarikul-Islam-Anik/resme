6ebd8366195547fc6ac6291fc08104f58c738cdc
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 00:06:54 +0600

    Change the intraction style.

--- app/dashboard/components/delete-account/deletion-warning.tsx
+++ app/dashboard/components/delete-account/deletion-warning.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { signOut } from 'next-auth/react';
 import { toast } from 'sonner';
@@ -16,13 +16,28 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import useMediaQuery from '@/hooks/use-media-query';
 
 import deleteUser from './action';
+
 
 interface DeleteAccountProps {
   children: React.ReactNode;
 }
 const DeletionWarning = ({ children }: DeleteAccountProps) => {
+  const [open, setOpen] = useState(false);
+  const { isDesktop } = useMediaQuery();
   const handleDeleteAccount = async () => {
     toast.promise(deleteUser(), {
       loading: 'Deleting account...',
@@ -35,29 +50,51 @@ const DeletionWarning = ({ children }: DeleteAccountProps) => {
       error: 'Could not delete account.',
     });
   };
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Delete your account</AlertDialogTitle>
-        </AlertDialogHeader>
-        <AlertDialogDescription>
-          Are you sure you want to delete your account? This action is permanent
-          and cannot be undone.
-        </AlertDialogDescription>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <form onSubmit={handleDeleteAccount}>
-            <AlertDialogAction type='submit' className='w-full'>
-              Delete
+
+  const title = 'Delete your account';
+  const description =
+    'Are you sure you want to delete your account? This action is permanent and cannot be undone.';
+
+  if (isDesktop) {
+    return (
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{title}</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction type='submit' asChild>
+              <Button variant='destructive' onClick={handleDeleteAccount}>
+                Delete
+              </Button>
             </AlertDialogAction>
-          </form>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  }
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className='text-left'>
+          <DrawerTitle>{title}</DrawerTitle>
+          <DrawerDescription>{description}</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button variant='destructive' onClick={handleDeleteAccount}>
+            Delete
+          </Button>
+          <DrawerClose asChild>
+            <Button variant='outline'>Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
-DeletionWarning.displayName = 'DeletionWarning';
 export default DeletionWarning;
