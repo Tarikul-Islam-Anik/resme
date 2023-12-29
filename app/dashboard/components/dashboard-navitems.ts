fc436dccbda6f300d7da681373fba21612038a1d
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 00:07:21 +0600

    Add dashboard navigation items

---
+++ app/dashboard/components/dashboard-navitems.ts
@@ -0,0 +1,34 @@
+export const dashboardNavItems = [
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+  },
+  {
+    title: 'Analytics',
+    href: '/dashboard/analytics',
+  },
+  {
+    title: 'Profile',
+    href: '/dashboard/profile',
+  },
+  {
+    title: 'Personal Information',
+    href: '/dashboard/personal-information',
+  },
+  {
+    title: 'Educational Informations',
+    href: '/dashboard/education',
+  },
+  {
+    title: 'Projects',
+    href: '/dashboard/projects',
+  },
+  {
+    title: 'Skills',
+    href: '/dashboard/skills',
+  },
+  {
+    title: 'Experiences',
+    href: '/dashboard/experience',
+  },
+];
