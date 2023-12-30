7bd168e9075ec5aef1973106cbd208d3d9cc0197
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 15:10:13 +0600

    Add ShareLink component

---
+++ app/dashboard/components/sharelink.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { toast } from 'sonner';
+
+import { Text } from '@/components/typography/text';
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+const ShareLink = ({ link }: { link: string }) => {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText('https://resme.vercel.app/' + link);
+    toast.success('Link is copied to clipboard!');
+  };
+  return (
+    <TooltipProvider delayDuration={300}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant='outline' onClick={copyToClipboard}>
+            <Text>Share link</Text>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <Text as='p'>Copy the link</Text>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+ShareLink.displayName = 'ShareLink';
+export default ShareLink;
